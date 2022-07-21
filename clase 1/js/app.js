bfbcbc7b63d30a25516aef4a4adf6f04a2af118c
Sofi1210 <zapatasofia814@gmail.com>
2022-07-21 08:10:03 -0500

    esta es la clase de hoy

---
+++ clase 1/js/app.js
@@ -0,0 +1,8 @@
+alert("hola mundo");
+//esto es una variable
+let nombre="Sofia"
+console.log(`hola ${nombre}`);
+
+//esto es una constante
+const correo="zapatasofia814@gmail.com"
+console.log(`este es el correo ${correo}`)
---
+++ clase 3/asests/app.js
@@ -0,0 +1,14 @@
+//crear un array vacio
+let array=[];
+
+console.log(array);
+//creamos 4 vectores con diferentes datos
+let numeros=[1,2,3,4]
+
+
+let animales=["perro","gato","pato","sapo"]
+//console.log(animales)
+let colores=["amarillo","rojo","verde","azul","morado"]
+//console.log(colores)
+let comida=["salchipapas","perro","arepa de queso"]
+console.table(comida)
