ff860732f9b3de007cb16837e67fbbd4238dc50f
Sofi1210 <zapatasofia814@gmail.com>
2022-07-22 09:22:04 -0500

    funciones nativas para string

---
+++ clase 4/asests/funcionesNativas.js
@@ -0,0 +1,13 @@
+let saludo="hola Sofi"
+
+console.log(saludo);
+console.log(saludo.length)//devuelve cantidad de caracteres
+console.log(saludo.toUpperCase())//devuelve el texto en mayusculas
+console.log(saludo.toLowerCase())//devuelve el texto en minusculas
+console.log(saludo.slice(0,4))//devuelve la posicion al numero indicado
+console.log(saludo.includes("mundo"))//devuelve verdadero o falso si la cadena existe
+console.log(saludo.indexOf("a"))//devuelve la primera posicion a la que aparezca la cadena ingresada por parametro
+console.log(saludo.replace("a" ,"s"))//permite reemplazar una letra por otra letra o un carcter
+
+let numero=5
+console.log(numero.toString())//este metodo convierte la variable en string
