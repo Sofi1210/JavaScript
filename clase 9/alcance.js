d54a044e0551c08ac9d7441247fb7218bb77bfd5
Sofi1210 <zapatasofia814@gmail.com>
2022-08-02 09:22:06 -0500

    alcance en js

---
+++ clase 9/alcance.js
@@ -0,0 +1,22 @@
+let num=1;
+
+
+console.log(num)
+
+const saludar=()=>{
+    let num2=5;
+    console.log(num*num2)
+}
+ 
+saludar()
+
+const validarEdad=()=>{
+    let edad=19;
+    if (edad>18){
+        let mensaje="Wakanda for ever"
+        console.log("es mayor de edad")
+        console.log(mensaje)
+    }else{
+        console.log("estas sardio")
+    }
+}
