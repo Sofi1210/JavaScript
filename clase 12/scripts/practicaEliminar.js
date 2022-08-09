c5bb067250131bb96d30e14524be96e7f40b6145
lean420 <juanpablocoloradoalvarez@gmail.com>
2022-08-09 08:53:46 -0500

    vimos como crear y eliminar elementos

---
+++ clase 12/scripts/practicaEliminar.js
@@ -0,0 +1,17 @@
+
+//let elementos=document.querySelectorAll(".elemento")
+
+//for (let i = 0; i < elementos.length; i++) {
+   //lista.removeChild(elementos[i])
+   
+    
+//}
+
+let lista=document.querySelector('#lista')
+
+for (let i = 1; i <= 10; i++) {
+ 
+    let elemento=document.createElement("li")
+    elemento.innerHTML=i
+    lista.appendChild(elemento)
+}
