1b2e26894676d0cb84ec2194e4736c7411e3cb9c
lean420 <juanpablocoloradoalvarez@gmail.com>
2022-08-11 07:27:56 -0500

    aqui vimos como manejar los errores

---
+++ clase 13/scripts/app.js
@@ -0,0 +1,12 @@
+try {
+    const nombre="sofia";
+    const apellidos="Zapata Pulgarin";
+const nombreCompleto= nombre + " " + apellidos;
+
+console.log(nombreCompleto);
+
+} catch (e) {
+    console.log(e)
+}
+
+
---
+++ clase 13/scripts/errorPersonalizado.js
@@ -0,0 +1,16 @@
+const obtenerNumero=()=>{
+    let num=prompt("ingrese un numero")
+    try {
+        if (num == "") throw "esta vacio"; 
+        if (isNaN(num)) throw "no es numero";
+        num = Number (num);
+        if (num <5) throw "el numero es muy pequeño";
+        if (num >10) throw "el numero es muy grande";
+             
+        
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+obtenerNumero()
